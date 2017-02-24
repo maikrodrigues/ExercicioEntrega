91c64afea9f63712d8c379fc89003cf0a39436ef
Lab <Lab@LAB0302>
2017-02-23 21:02:22 -0300

    Add project files.

---
+++ ExercicioEntrega/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioEntrega
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TextReader reader = new StreamReader("nomes_telefones.txt");
+            Console.WriteLine(reader);
+
+            String linha = reader.ReadLine();
+            
+            while (linha != null)
+            {
+                linha = linha.Replace("-", "").Replace("(", "").Replace(")", "");
+                Console.WriteLine(linha);
+                linha = reader.ReadLine();
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
